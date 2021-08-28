6d9e1b47c184ebad761aa0b4b9126cb378e83b65
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-08-28 12:14:05 +0300

    fixed decoding problems

--- Sources/OptikTGSwiftSDK/Structs/Message.swift
+++ Sources/OptikTGSwiftSDK/Structs/Message.swift
@@ -14,21 +14,4 @@ public struct Message : Codable {
         public let new_chat_member: User?
         public let new_chat_members: [User]?
         public let entities: [Entitie]?
-
-        private enum Keys : String, CodingKey {
-                case message_id, from, chat, date, text, entities, new_chat_member, new_chat_participant, new_chat_members
-        }
-
-        public init(from decoder: Decoder) throws {
-                let container = try decoder.container(keyedBy: Keys.self)
-                message_id = try container.decode(UInt.self, forKey: .message_id)
-                from = try container.decode(From.self, forKey: .from)
-                chat = try container.decode(Chat.self, forKey: .chat)
-                date = try  container.decode(Date.self, forKey: .date)
-                text = try container.decode(String?.self, forKey: .text)
-                entities = try container.decode([Entitie]?.self, forKey: .entities)
-                new_chat_participant = try container.decode(User?.self, forKey: .new_chat_participant)
-                new_chat_member = try container.decode(User?.self, forKey: .new_chat_member)
-                new_chat_members = try container.decode([User]?.self, forKey: .new_chat_members)
-        }
 }
