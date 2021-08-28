09a28381f8775f700d6371897169e0a7c2611868
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-08-28 12:15:44 +0300

    fixed decoding problems

--- Sources/OptikTGSwiftSDK/Structs/Update.swift
+++ Sources/OptikTGSwiftSDK/Structs/Update.swift
@@ -8,15 +8,4 @@ public struct Update : Codable {
     public let update_id: UInt
     public let message: Message?
     public let my_chat_member: ChatMember?
-
-    private enum Keys: String, CodingKey {
-        case update_id, message, my_chat_member
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: Keys.self)
-        update_id = try container.decode(UInt.self, forKey: .update_id)
-        message = try container.decode(Message.self, forKey: .message)
-        my_chat_member = try container.decode(ChatMember.self, forKey: .my_chat_member)
-    }
 }
